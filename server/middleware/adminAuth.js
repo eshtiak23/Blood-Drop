a86fc3f73c3310991ec8a054294787eb1bc3e1f4
eshtiak23 <eshtiaktushar@gmial.com>
2026-07-18 13:03:37 +0600

    refactor: remove server folder (now separate repo Blood-Drop_Server)

--- server/middleware/adminAuth.js
+++
@@ -1,6 +0,0 @@
-const adminAuth = (req, res, next) => {
-  if (req.user.role !== "admin") return res.status(403).json({ error: "Admin access required" });
-  next();
-};
-
-export default adminAuth;
